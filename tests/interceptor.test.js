29161974c9543a0189a4724799cf4c18cf646782
Mikael Karon <mikael@karon.se>
2024-03-19 23:41:36 +0100

    test: add test for non-interceted request

--- tests/interceptor.test.js
+++ tests/interceptor.test.js
@@ -7,7 +7,40 @@ const { request, Agent } = require('undici')
 const { ServerDigestAuth, QOP_AUTH, QOP_AUTH_INT } = require('@mreal/digest-auth')
 const { createDigestInterceptor } = require('../')
 
-test('make a request without auth', async (t) => {
+test('make a request without auth (not intercepted)', async (t) => {
+  const server = http.createServer((req, res) => {
+    requestCount++
+    res
+      .writeHead(200)
+      .end()
+  })
+  server.listen(0)
+
+  t.after(() => server.close())
+
+  const targetUrl = `http://localhost:${server.address().port}`
+  const username = 'username'
+  const password = 'password'
+  let requestCount = 0
+
+  const dispatcher = new Agent({
+    keepAliveTimeout: 10,
+    keepAliveMaxTimeout: 10,
+    interceptors: {
+      Pool: [createDigestInterceptor({
+        username,
+        password
+      })]
+    }
+  })
+
+  const { statusCode } = await request(targetUrl, { dispatcher })
+
+  assert.strictEqual(statusCode, 200)
+  assert.strictEqual(requestCount, 1)
+})
+
+test('make a request without auth (intercepted)', async (t) => {
   const server = http.createServer((req, res) => {
     requestCount++
     res
